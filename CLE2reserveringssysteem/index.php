ee25dd19d437ff2a51de54f89bcbacd2a8fb8f2b
Skye Jacobs <skyejacobs@casema.nl>
2025-01-07 12:14:46 +0100

    Basis index aangemaakt

--- CLE2reserveringssysteem/index.php
+++ CLE2reserveringssysteem/index.php
@@ -3,9 +3,65 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Document</title>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <title>Homepage</title>
 </head>
 <body>
-
+<nav>
+    <!-- insert nav here-->
+</nav>
+<header>
+    <h1>Auniek Interieur</h1>
+    <p>De unieke uitstraling die jouw huis verdiend!</p>
+</header>
+<main>
+    <section>
+        <h2>Onze werkwijzen</h2>
+        <article>
+            <h3>Nieuwbouw</h3>
+            <div class="button">
+                <a href="#">Meer lezen</a>
+            </div>
+        </article>
+        <article>
+            <h3>Bestaande bouw</h3>
+            <div class="button">
+                <a href="#">Meer lezen</a>
+            </div>
+        </article>
+    </section>
+    <section>
+        <h2>Reviews</h2>
+        <article>
+            <h3>Harry de Jong</h3>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Animi cum deleniti dicta dolorem error facilis
+                illum inventore laborum natus nihil nobis, nulla perferendis porro provident quibusdam similique sit
+                temporibus vel.</p>
+            <div>
+                <span class="fa fa-star checked"></span>
+                <span class="fa fa-star checked"></span>
+                <span class="fa fa-star checked"></span>
+                <span class="fa fa-star checked"></span>
+                <span class="fa fa-star"></span>
+            </div>
+        </article>
+        <article>
+            <h3>ILse Vermolen</h3>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aspernatur aut consequatur consequuntur dolore
+                dolorem impedit ipsam maiores officia quasi, quis soluta unde voluptas. At sunt ullam unde. Accusantium,
+                cupiditate exercitationem!</p>
+            <div>
+                <span class="fa fa-star checked"></span>
+                <span class="fa fa-star checked"></span>
+                <span class="fa fa-star checked"></span>
+                <span class="fa fa-star checked"></span>
+                <span class="fa fa-star"></span>
+            </div>
+        </article>
+    </section>
+</main>
+<footer>
+    <!-- insert footer here-->
+</footer>
 </body>
 </html>
