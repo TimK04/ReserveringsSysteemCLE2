511de92934b16e80eb356ce3cef119d5d3975e82
tnvanbockel@gmail.com <tnvanbockel@gmail.com>
2025-01-07 11:47:54 +0100

    making form for login.php.

--- CLE2reserveringssysteem/login.php
+++ CLE2reserveringssysteem/login.php
@@ -10,10 +10,16 @@
     <title>Document</title>
 </head>
 <body>
-<form>
+<h1> Inloggen *bedrijf* </h1>
+<form method="post" action="">
 
+    <label for="email">E-mail</label>
+    <input id="email" type="email" name="email">
 
-    <button>Login</button>
+    <label for="password">wachtwoord</label>
+    <input id="password" type="password" name="password">
+
+    <button type="submit" name="submit">Login</button>
 </form>
 </body>
 </html>
