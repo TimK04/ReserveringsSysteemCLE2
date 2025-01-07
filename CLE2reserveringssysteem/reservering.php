391d50165d2d90c44c0d9da2cfb880cbb93683c1
chrisieee <0998374@hr.nl>
2025-01-07 13:57:11 +0100

    Updates reservering

---
+++ CLE2reserveringssysteem/reservering.php
@@ -0,0 +1,52 @@
+<?php
+
+?>
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="css/style.css">
+    <title>Document</title>
+</head>
+<body>
+<nav>
+    <div class="nav_img">
+        <img src="images/logo.png" alt="logo Auniek Interieur">
+    </div>
+    <div class="empty"></div>
+    <div class="nav_links">
+        <a href="index.php">Home</a>
+        <a href="">Over Auniek</a>
+        <a href="">Portfolio</a>
+        <a href="reservering.php">Intake</a>
+        <a href="">Werkwijzen</a>
+        <a href="">Blog</a>
+        <a href="">Contact</a>
+    </div>
+</nav>
+<main>
+    <h1>Intake gesprek inplannen</h1>
+</main>
+<footer>
+    <div class="nav_img">
+        <img src="images/logo.png" alt="logo Auniek Interieur">
+    </div>
+    <div class="information">
+        <p>Informatie</p>
+        <a href="">Algemene voorwaarden</a>
+        <a href="">Privacy policy</a>
+    </div>
+    <div class="contact">
+        <p>Contact</p>
+        <div>
+            <a href="https://nl.pinterest.com/auniekinterieurs/"><img src="images/pinterest.png" alt="Pinterest"></a>
+            <a href="https://www.instagram.com/auniek_interieurs/profilecard/?igsh=czk1ODl0bzF1OTl2"><img
+                        src="images/instagram.png" alt="Instagram"></a>
+        </div>
+    </div>
+</footer>
+</body>
+</html>
