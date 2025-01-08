7796ab87755ea61a66f678f05353b8c038d9583c
Skye Jacobs <skyejacobs@casema.nl>
2025-01-08 11:46:32 +0100

    Gedeelde style uit home

--- CLE2reserveringssysteem/index.php
+++ CLE2reserveringssysteem/index.php
@@ -10,7 +10,24 @@
 </head>
 <body>
 <nav>
-    <!-- insert nav here-->
+    <div class="nav_img">
+        <img src="images/logo.png" alt="logo Auniek Interieur">
+    </div>
+    <div class="empty"></div>
+    <div class="nav_links">
+        <a href="index.php">Home</a>
+        <a href="overons.php">Over Auniek</a>
+        <a href="portfolio.php">Portfolio</a>
+        <a href="reservering.php">Intake</a>
+        <a href="werkwijzen.php">Werkwijzen</a>
+        <a href="blog.php">Blog</a>
+        <a href="contact.php">Contact</a>
+    </div>
+    <img class="dropdown_container" src="images/inlog.png" alt="Profiel">
+    <div class="dropdown">
+        <a href="">Profiel</a>
+        <a href="">Logout</a>
+    </div>
 </nav>
 <header>
     <h1>Auniek Interieur</h1>
@@ -74,7 +91,34 @@
     </section>
 </main>
 <footer>
-    <!-- insert footer here-->
+    <div class="nav_img">
+        <img src="images/logo.png" alt="logo Auniek Interieur">
+    </div>
+    <div class="categorieën">
+        <p class="underlined">Categorieën</p>
+        <a href="index.php">Home</a>
+        <a href="overons.php">Over Auniek</a>
+        <a href="portfolio.php">Portfolio</a>
+        <a href="reservering.php">Intake</a>
+        <a href="werkwijzen.php">Werkwijzen</a>
+        <a href="blog.php">Blog</a>
+        <a href="contact.php">Contact</a>
+    </div>
+    <div class="information">
+        <p class="underlined">Informatie</p>
+        <a href="">Algemene voorwaarden</a>
+        <a href="">Privacy policy</a>
+    </div>
+    <div class="contact">
+        <p class="underlined">Contact</p>
+        <p>Tel: 06-48729036</p>
+        <p>Email: info@auniekinterieurs.nl</p>
+        <div>
+            <a href="https://nl.pinterest.com/auniekinterieurs/"><img src="images/pinterest.png" alt="Pinterest"></a>
+            <a href="https://www.instagram.com/auniek_interieurs/profilecard/?igsh=czk1ODl0bzF1OTl2"><img
+                        src="images/instagram.png" alt="Instagram"></a>
+        </div>
+    </div>
 </footer>
 </body>
 </html>
