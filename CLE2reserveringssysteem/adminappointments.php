cea4eb07f7d94f4958a3622bb7669159a2a5d831
TimK04 <kalkmantim04@gmail.com>
2025-01-10 14:55:18 +0100

    added adminappointments.php

---
+++ CLE2reserveringssysteem/adminappointments.php
@@ -0,0 +1,72 @@
+<?php
+/** @var mysqli $db */
+require_once "include/database.php";
+
+$query = "SELECT * FROM reservations";
+
+$result = mysqli_query($db, $query)
+or die('Error ' . mysqli_error($db) . ' with query ' . $query);
+
+$reservations = [];
+
+while ($row = mysqli_fetch_assoc($result)) {
+    $reservations[] = $row;
+}
+
+mysqli_close($db);
+?>
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Admin Reserveringen</title>
+    <link rel="stylesheet" href="css/style.css">
+</head>
+<body>
+<?php require_once 'include/nav.php' ?>
+<header>
+    <h1>Admin Reserveringen</h1>
+</header>
+<main>
+
+    <a  href="admin.php">Admin Home</a>
+
+    <section>
+        <table>
+            <thead>
+            <tr>
+                <th>Positie</th>
+                <th>Voornaanm</th>
+                <th>Achternaam</th>
+                <th>Email</th>
+                <th>Tijd</th>
+                <th>Datum</th>
+                <th>text</th>
+            </tr>
+            </thead>
+            <tbody>
+            <?php foreach ($reservations as $index => $reservations): ?>
+                <tr>
+                    <th><?= $index + 1; ?></th>
+                    <th><?= $reservations['first_name']; ?></th>
+                    <th><?= $reservations['last_name']; ?></th>
+                    <th><?= $reservations['email']; ?></th>
+                    <th><?= $reservations['time']; ?></th>
+                    <th><?= $reservations['date']; ?></th>
+                    <th><?= $reservations['text']; ?></th>
+                    <th>
+                        <a href="adminappointmentsedit.php?id= <?=$reservations['id']?>" href="">Reserveringen Aanpassen</a>
+                    </th>
+                </tr>
+            <?php endforeach; ?>
+            </tbody>
+        </table>
+    </section>
+
+</main>
+<?php require_once 'include/footer.php' ?>
+</body>
+</html>
