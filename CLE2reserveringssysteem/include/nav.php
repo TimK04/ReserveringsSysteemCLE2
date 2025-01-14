c07f521e05c8a3ea8f10c6eee83f2093d709a603
chrisieee <0998374@hr.nl>
2025-01-14 15:52:28 +0100

    Nav verbeteringen

--- CLE2reserveringssysteem/include/nav.php
+++ CLE2reserveringssysteem/include/nav.php
@@ -5,7 +5,7 @@
     <div class="empty"></div>
     <div class="nav_links">
         <a href="index.php">Home</a>
-        <a href="overons.php">Over Auniek</a>
+        <a class="about_us" href="overons.php">Over Auniek</a>
         <a href="portfolio.php">Portfolio</a>
         <a href="reservering.php">Intake</a>
         <div class="dropdown">
@@ -18,7 +18,7 @@
         <a href="blog.php">Blog</a>
         <a href="contact.php">Contact</a>
     </div>
-    <div class="dropdown">
+    <div class="dropdown profile">
         <a href=""><img class="logo" src="images/inlog.png" alt="Profiel"></a>
         <div class="dropdown_items" id="person">
             <a href="profile.php">Profiel</a>
