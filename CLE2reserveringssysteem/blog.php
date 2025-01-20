f1e46b1e623de68202e65d4c36f1cfa9575fb458
Skye Jacobs <skyejacobs@casema.nl>
2025-01-20 14:54:18 +0100

    Kleine veranderingen aan pagina's die nog gemaakt moeten worden

--- CLE2reserveringssysteem/blog.php
+++ CLE2reserveringssysteem/blog.php
@@ -11,15 +11,19 @@ session_start();
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <link rel="stylesheet" href="css/style.css">
-    <link rel="stylesheet" href="css/intake.css">
+    <link rel="stylesheet" href="css/bouw.css">
+    <link rel="stylesheet" href="css/media.css">
     <title>Auniek Interieur - Blog</title>
 </head>
 <body>
 <div id="contentnav">
 </div>
 <script src="include/screensize.js"></script>
+<header>
+    <h1>Blog</h1>
+</header>
 <main>
-    <h1>Coming soon!</h1>
+    <h2>Coming soon!</h2>
 </main>
 <div id="contentfooter">
 </div>
